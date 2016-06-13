4b23fa0d3062617b6af96e9c0a532645ed52abcd
Greg Bronevetsky <greg@bronvetsky.com>
2016-06-12 21:10:18 -0600

    Greg's changes to test driver synthesis

--- projects/fuse/src/driverNIG.py
+++ projects/fuse/src/driverNIG.py
@@ -14,9 +14,11 @@ def main(argv):
     for funcDepth in range(4) :
       for ifPerFuncDepth in range(4) :
         for globalVars in [0,1] :
+          #print "/g/g15/bronevet/Compilers/rose/projects/fuse/src/nestedIfGen.py nestedIf.C "+str(funcDepth)+" "+str(ifPerFuncDepth)+" "+str(globalVars)+" "+str(constInitVars)
           os.system("/g/g15/bronevet/Compilers/rose/projects/fuse/src/nestedIfGen.py nestedIf.C "+str(funcDepth)+" "+str(ifPerFuncDepth)+" "+str(globalVars)+" "+str(constInitVars))
           proc = subprocess.Popen(["./fuse", "nestedIf.C"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
           for line in iter(proc.stdout.readline, b''):
+            #print line 
             if line.find("TightComposer Elapsed") >=0 :
               line = line.rstrip('\n')
               fields = line.split("=")

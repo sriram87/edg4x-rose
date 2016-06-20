515fedabca3f8cf5a46e3728570d4eaa00d321b8
Sriram Aananthakrishnan <sriram@cs.utah.edu>
2016-06-20 10:54:05 -0600

    (fuse-mpi) isolated mpi utils into a separate file

---
+++ projects/fuse/src/simpleAnalyses/mpi_utils.C
@@ -0,0 +1,318 @@
+#include "sage3basic.h"
+#include "mpi_utils.h"
+#include "sight.h"
+#include "sight_control.h"
+
+using namespace std;
+using namespace sight;
+
+namespace fuse {
+  /*************
+   * MPICommOp *
+   *************/
+  MPICommOp::MPICommOp(const Function& func) {
+    string name = func.get_name().getString();
+    if(name.compare("MPI_Send") == 0) optype = MPICommOp::SEND;
+    else if(name.compare("MPI_Recv") == 0) optype = MPICommOp::RECV;
+    else if(name.compare("MPI_Barrier") == 0) optype = MPICommOp::BARRIER;
+    else if(name.compare("MPI_Comm_rank") == 0) optype = MPICommOp::COMMRANK;
+    else if(name.compare("MPI_Comm_rank") == 0) optype = MPICommOp::COMMSIZE;
+    else if(name.compare("MPI_Bcast") == 0) optype = MPICommOp::BCAST;
+    else if(name.compare("MPI_Init") == 0) optype = MPICommOp::INIT;
+    else if(name.compare("MPI_Finalize") == 0) optype = MPICommOp::FINALIZE;
+    else optype = MPICommOp::UNKNOWN;
+  }
+
+  MPICommOp::MPICommOp(const MPICommOp& that) : optype(that.optype) {
+  }
+
+  bool MPICommOp::isMPISendOp() const {
+    return optype == MPICommOp::SEND;
+  }
+
+  bool MPICommOp::isMPIRecvOp() const {
+    return optype == MPICommOp::RECV;
+  }
+
+  bool MPICommOp::isMPIBarrierOp() const {
+    return optype == MPICommOp::BARRIER;
+  }
+
+  bool MPICommOp::isMPIBcastOp() const {
+    return optype == MPICommOp::BCAST;
+  }
+
+  bool MPICommOp::isMPIUnknownOp() const {
+    return optype == MPICommOp::UNKNOWN;
+  }
+
+  /*********************
+   * MPISendOpCallSite *
+   *********************/
+  MPISendOpCallSite::MPISendOpCallSite(const Function& mpif_,
+                                       SgFunctionCallExp* sgn)
+    : mpif_(mpif_), sgn(sgn) { }
+
+  // Traverse the expression tree to find the buffer expr for the MPI operations
+  SgExpression* traverseAST(SgExpression* expr) {
+    switch(expr->variantT()) {
+    case V_SgVarRefExp:
+      return expr;
+    case V_SgAddressOfOp:
+      return traverseAST(isSgUnaryOp(expr)->get_operand());
+    case V_SgCastExp:
+      return traverseAST(isSgUnaryOp(expr)->get_operand());
+    case V_SgPntrArrRefExp:
+      return expr;
+    case V_SgIntVal:
+      return expr;
+    default:
+      cerr << "Unhandled buffer Expr in traverseATS(expr=" 
+           << SgNode2Str(expr) << ")\n"
+           << ", file:" << __FILE__
+           << ",line: " << __LINE__ << endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  SgNode* MPISendOpCallSite::getSendBuffer() {
+    SgExpressionPtrList& args = sgn->get_args()->get_expressions();
+    SgExpression* bexpr = traverseAST(args[0]); assert(bexpr);
+    return bexpr;
+  }
+
+  bool MPISendOpCallSite::isSendOp() {
+    string name = mpif_.get_name().getString();
+    return (name.compare("MPI_Send") == 0);          
+  }
+  
+  /*********************
+   * MPIRecvOpCallSite *
+   *********************/
+  MPIRecvOpCallSite::MPIRecvOpCallSite(const Function& mpif_,
+                                       SgFunctionCallExp* sgn)
+    : mpif_(mpif_), sgn(sgn) { }
+
+  SgNode* MPIRecvOpCallSite::getRecvBuffer() {
+    SgExpressionPtrList& args = sgn->get_args()->get_expressions();
+    SgExpression* bexpr = traverseAST(args[0]); assert(bexpr);
+    return bexpr;
+  }
+
+  bool MPIRecvOpCallSite::isRecvOp() {
+    string name = mpif_.get_name().getString();
+    return (name.compare("MPI_Recv") == 0);
+            
+  }
+
+  /**********************
+   * MPIBcastOpCallSite *
+   **********************/
+  MPIBcastOpCallSite::MPIBcastOpCallSite(const Function& mpif_,
+                                         SgFunctionCallExp* sgn)
+    : mpif_(mpif_), sgn(sgn) { }
+
+  SgNode* MPIBcastOpCallSite::getBcastBuffer() {
+    SgExpressionPtrList& args = sgn->get_args()->get_expressions();
+    SgExpression* bexpr = traverseAST(args[0]); assert(bexpr);
+    return bexpr;
+  }
+
+  SgNode* MPIBcastOpCallSite::getBcastRootExpr() {
+    SgExpressionPtrList& args = sgn->get_args()->get_expressions();
+    SgExpression* rexpr = traverseAST(args[3]); assert(rexpr);
+    return rexpr;
+  }
+
+  /*******************
+   * ValueObject2Int *
+   *******************/
+  typedef boost::shared_ptr<SgValueExp> SgValueExpPtr;
+  typedef std::set<SgValueExpPtr> SgValueExpPtrSet;
+
+  ValueObject2Int::ValueObject2Int(Composer* composer, PartEdgePtr pedge, 
+                                   ComposedAnalysis* analysis, int debugLevel)
+  : composer(composer), 
+    pedge(pedge), 
+    analysis(analysis),
+    debugLevel(debugLevel) { }
+
+  int ValueObject2Int::operator()(SgNode* sgn) {
+    SIGHT_VERB_DECL(scope, ("ValueObject2Int", scope::low),
+                    3, debugLevel) 
+    SIGHT_VERB(dbg << "sgn=" << SgNode2Str(sgn) << endl, 3, debugLevel)
+    SIGHT_VERB(dbg << "pedge=" << pedge->str() << endl, 3, debugLevel)
+
+    ValueObjectPtr vo = composer->Expr2Val(sgn, pedge, analysis);
+    SIGHT_VERB(dbg << vo->str() << endl, 3, debugLevel)
+
+      assert(vo->isConcrete());
+
+    SgValueExpPtrSet cvalues = vo->getConcreteValue();
+    SIGHT_VERB_IF(2, debugLevel)
+      SgValueExpPtrSet::iterator it = cvalues.begin();
+    for(int i = 0; it != cvalues.end(); ++it, ++i) {
+      dbg << "cvalues[" << i << "]=" << SgNode2Str(it->get()) << endl;
+    }
+    SIGHT_VERB_FI()
+      assert(cvalues.size() == 1);
+    SgValueExpPtr sgval = *cvalues.begin();
+
+    switch(sgval.get()->variantT()) {      
+    case V_SgIntVal: {
+      return isSgIntVal(sgval.get())->get_value();
+    }
+    case V_SgLongIntVal: {
+      return isSgLongIntVal(sgval.get())->get_value();
+    }
+    case V_SgLongLongIntVal: {
+      return isSgLongLongIntVal(sgval.get())->get_value();
+    }
+      // Dont't know how MPI arguments are promoted to this type
+      // but a concrete evidence was seen in heat_mpi.c 
+    case V_SgDoubleVal: {
+      return (int)isSgDoubleVal(sgval.get())->get_value();
+    }
+      // TODO: Fill out cases for other int types
+    default: {
+      SIGHT_VERB(dbg << "unhandled type=" << SgNode2Str(sgval.get()) << endl, 3, debugLevel)
+        assert(0);      
+    } // end default
+    } // end switch case
+  }
+
+
+  /*********************
+   * Utility Functions *
+   *********************/
+  void unsupportedMPICommOpExit(int line) {
+    cerr << "Unsupported MPI Function at line: " << line << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  void usageErrorExit(int line) {
+    cerr << "Function Usage Error at line:" << line << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  SgInitializedName* getSendOpTarget(Function mpifunc) {
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPISendOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Send operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* target = arglist[3]; assert(target);
+    return target;
+  }
+
+  SgInitializedName* getSendOpTag(Function mpifunc) {
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPISendOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Send operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* tag = arglist[4]; assert(tag);
+    return tag;
+  }
+
+  SgInitializedName* getSendOpComm(Function mpifunc) { 
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPISendOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Send operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* comm = arglist[5]; assert(comm);
+    return comm;
+  }
+
+  SgInitializedName* getRecvOpSource(Function mpifunc) { 
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIRecvOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Recv operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* source = arglist[3]; assert(source);
+    return source;
+  }
+
+  SgInitializedName* getRecvOpTag(Function mpifunc) { 
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIRecvOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Recv operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* tag = arglist[4]; assert(tag);
+    return tag;
+  }
+
+  SgInitializedName* getRecvOpComm(Function mpifunc) {
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIRecvOp()) usageErrorExit(__LINE__);
+   
+    // We are processing MPI_Recv operation
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* comm = arglist[5]; assert(comm);
+    return comm;
+  }  
+
+  SgInitializedName* getBarrierComm(Function mpifunc) { 
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIBarrierOp()) usageErrorExit(__LINE__);
+    
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* comm = arglist[0]; assert(comm);
+    return comm;    
+  }
+
+  SgInitializedName* getBcastOpRoot(Function mpifunc) {
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIBcastOp()) usageErrorExit(__LINE__);
+    
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* root = arglist[3]; assert(root);
+    return root;
+  }
+
+  SgInitializedName* getBcastOpComm(Function mpifunc) {
+    MPICommOp op(mpifunc);
+    if(op.isMPIUnknownOp()) unsupportedMPICommOpExit(__LINE__);
+    if(!op.isMPIBcastOp()) usageErrorExit(__LINE__);
+    
+    SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+    SgInitializedNamePtrList& arglist = *arglist_p;
+    SgInitializedName* comm = arglist[4]; assert(comm);
+    return comm;
+  }
+
+  // SgPointerDerefExp* MPICommOp::getCommOpBufferDerefExpr() const {
+  //   SgPointerDerefExp* buffDerefExpr;
+  //   if(isMPICommSendOp() || isMPICommRecvOp()) {
+  //     SgInitializedNamePtrList* arglist_p = mpifunc.get_args();
+  //     SgInitializedNamePtrList& arglist = *arglist_p;
+  //     SgVariableSymbol* buffSymbol = isSgVariableSymbol(arglist[0]->search_for_symbol_from_symbol_table());
+  //     assert(buffSymbol);
+  //     SgVarRefExp* buffVarRefExpr = SageBuilder::buildVarRefExp(buffSymbol);
+  //     buffDerefExpr = SageBuilder::buildUnaryExpression<SgPointerDerefExp>(buffVarRefExpr);
+  //     assert(buffDerefExpr);
+  //   }
+  //   else assert(false);
+  //   return buffDerefExpr;
+  // }
+
+} // end namespace
---
+++ projects/fuse/src/simpleAnalyses/mpi_utils.h
@@ -0,0 +1,103 @@
+#ifndef _MPI_UTILS_H
+#define _MPI_UTILS_H
+
+#include "CallGraphTraverse.h"
+#include "compose.h"
+
+namespace fuse {
+
+  /*********
+   * MPIOp *
+   *********/
+  //! List of Supported MPI Operations in the ParFuse framework
+  class MPICommOp {
+    enum OpType {
+      COMMRANK,
+      COMMSIZE,
+      SEND,
+      RECV,
+      BARRIER,
+      BCAST,
+      INIT,
+      FINALIZE,
+      UNKNOWN
+    };
+    OpType optype;
+  public:
+    MPICommOp(const Function& func);
+    MPICommOp(const MPICommOp& that);
+    bool isMPISendOp() const;
+    bool isMPIRecvOp() const;
+    bool isMPIBarrierOp() const;
+    bool isMPIBcastOp() const;
+    bool isMPIUnknownOp() const;
+  };
+
+  /*********************
+   * MPISendOpCallSite *
+   *********************/
+  class MPISendOpCallSite {
+    const Function mpif_;
+    SgFunctionCallExp* sgn;
+  public:
+    MPISendOpCallSite(const Function& mpif_, SgFunctionCallExp* sgn);
+    //! Return syntactic expression corresponding to send buffer
+    SgNode* getSendBuffer();
+    bool isSendOp();
+  };
+
+  /*********************
+   * MPIRecvOpCallSite *
+   *********************/
+  class MPIRecvOpCallSite {
+    const Function mpif_;
+    SgFunctionCallExp* sgn;
+  public:
+    MPIRecvOpCallSite(const Function& mpif_, SgFunctionCallExp* sgn);
+    SgNode* getRecvBuffer();
+    bool isRecvOp();
+  };
+
+  /**********************
+   * MPIBcastOpCallSite *
+   **********************/
+  class MPIBcastOpCallSite {
+    const Function mpif_;
+    SgFunctionCallExp* sgn;
+  public:
+    MPIBcastOpCallSite(const Function& mpif_, SgFunctionCallExp* sgn);
+    SgNode* getBcastBuffer();
+    SgNode* getBcastRootExpr();
+  };
+
+  /*******************
+   * ValueObject2Int *
+   *******************/
+  class ValueObject2Int {
+    Composer* composer;
+    PartEdgePtr pedge;
+    ComposedAnalysis* analysis;
+    int debugLevel;
+  public:
+    ValueObject2Int(Composer* composer, PartEdgePtr pedge, 
+                    ComposedAnalysis* analysis, int debugLevel);
+    int operator()(SgNode* sgn);
+  };
+
+  //! Traverse the expression tree to find the buffer expr for the MPI operations
+  SgExpression* traverseAST(SgExpression* expr);
+  SgInitializedName* getSendOpTarget(Function function);
+  SgInitializedName* getSendOpTag(Function function);
+  SgInitializedName* getSendOpComm(Function function);
+
+  SgInitializedName* getRecvOpSource(Function function);
+  SgInitializedName* getRecvOpTag(Function function);
+  SgInitializedName* getRecvOpComm(Function function);  
+
+  SgInitializedName* getBarrierComm(Function function);
+
+  SgInitializedName* getBcastOpRoot(Function function);
+  SgInitializedName* getBcastOpComm(Function function);
+};
+
+#endif
